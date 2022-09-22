9593fc539b891b2d773f666be5ceca9e247c5f24
温昕亮 <xinliang.wen.2001@stu.moonshotacademy.cn>
2022-09-22 12:40:02 +0800

    -

--- Exam/Exam2/3.py
+++ Exam/Exam2/3.py
@@ -1,9 +1,9 @@
 m = int(input("请输入一个数字："))
 n = int(input("请输入另一个数字："))
 i = 0
-while i < n:
+c = 0
+while i < n + 1:
     a = m * (((10 ** i) - 1)/(10 - 1))
     i += 1
-    b = m * (((10 ** i) - 1)/(10 - 1))
-    c = a + b
+    c += a
 print(c)
