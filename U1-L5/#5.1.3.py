a774d134892ad7c0da27d3a0bbee2ee15efbe152
温昕亮 <xinliang.wen.2001@stu.moonshotacademy.cn>
2022-09-29 10:58:58 +0800

    课上的改

--- U1-L5/#5.1.3.py
+++ U1-L5/#5.1.3.py
@@ -1,11 +1,14 @@
 # 5.1.3 依次删除下方列表中所有的 3，并在删除后打印每个 3 的索引，最后打印最终的列表
 list_num = [1, 3, 4, 3, 7, 3, 9, 8, 6, 3]
+list2_num = [1, 3, 4, 3, 7, 3, 9, 8, 6, 3]
 for i in range(0, len(list_num)):
     if list_num[i] == 3 and i < 9:
         print(i, end=" ")
+        list2_num.remove(3)
     if list_num[i] == 3 and i == 9:
         print(i)
-print(list_num)
+        list2_num.remove(3)
+print(list2_num)
 
 
 
