fdf7ed4b1b0add897ab93686ffa6b8ab0e925166
温昕亮 <xinliang.wen.2001@stu.moonshotacademy.cn>
2022-10-01 21:34:15 +0800

    最后一个issue

--- Exam/Exam2/2.py
+++ Exam/Exam2/2.py
@@ -1,16 +1,17 @@
-a = float(input("请输入一个数字："))
-b = float(input("请输入另一个数字："))
-c = input("你希望对这两个数怎么操作（+，-，*，/）：")
-if c == "+":
-    print(a+b)
-elif c == "-":
-    print(a-b)
-elif c == "*":
-    print(a*b)
-elif c == "/":
-    if b == 0:
-        print("0 不可以作为除数哦~")
-    else:
-        print(a/b)
+while 1:
+    a = float(input("请输入一个数字："))
+    b = float(input("请输入另一个数字："))
+    c = input("你希望对这两个数怎么操作（+，-，*，/）：")
+    if c == "+":
+        print(a+b)
+    elif c == "-":
+        print(a-b)
+    elif c == "*":
+        print(a*b)
+    elif c == "/":
+        if b == 0:
+            print("0 不可以作为除数哦~")
+        else:
+            print(a/b)
 
 
