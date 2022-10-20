b35fb6b8a0a7e3773d86b55107145f10c545c29c
温昕亮 <xinliang.wen.2001@stu.moonshotacademy.cn>
2022-10-20 23:51:36 +0800

    我成功啦！！！！！！！！！

--- U1-L6/#6.2.6.py
+++ U1-L6/#6.2.6.py
@@ -1,12 +1,23 @@
 correspond_dict = {}
 for i in range(0, 26):
-    correspond_dict[chr(65 + i)] = i + 1
-print(correspond_dict)
+    correspond_dict[i] = chr(65 + i)
+# print(correspond_dict)
 
 
 def return_excel(columnNumber):
     a = ""
-    a += correspond_dict([(return_excel(columnNumber) - 1) % 26])
-    return a
+    b = -1
+    while 1:
+        if columnNumber == 0:
+            break
+        if b != (columnNumber - 1) % 26 or columnNumber >= 26:
+            b = (columnNumber - 1) % 26
+            a += correspond_dict[(columnNumber - 1) % 26]
+        elif (columnNumber - 1) == (columnNumber - 1) % 26 or (columnNumber - 1) % 26 == 0:
+            a += correspond_dict[(columnNumber - 1) % 26]
+            break
+        columnNumber = (columnNumber - 1) // 26
+    return a[::-1]
 
-print(return_excel(26))
+
+print(return_excel(92834))
