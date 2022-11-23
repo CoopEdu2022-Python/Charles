6863054896bd91c8678617925259d880efe2a722
温昕亮 <xinliang.wen.2001@stu.moonshotacademy.cn>
2022-11-23 10:04:32 +0800

    改

--- Exam/Exam8/5.py
+++ Exam/Exam8/5.py
@@ -1,10 +1,14 @@
 def largest_perimeter(nums: list) -> int:
     nums.sort(reverse=True)
-    if (nums[1] + nums[2] > nums[0]) and (nums[0] - nums[2]):
-        return sum(nums)
+    print(nums)
+    for i in range(2, len(nums)):
+        if nums[1] + nums[i] > nums[0]:
+            if nums[0] - nums[i] < nums[1]:
+                a = nums[0] + nums[1] + nums[i]
+                return a
     else:
         return 0
 
 
-print(largest_perimeter([1, 2, 1]))
+print(largest_perimeter( [2,1,5,6,9,10,13,7,7,3,2,5,4]))
 print(largest_perimeter([2, 1, 2]))
