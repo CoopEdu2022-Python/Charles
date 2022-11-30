4c2f3dbef55332a6bf7b267a80a5c9795c914a8c
温昕亮 <xinliang.wen.2001@stu.moonshotacademy.cn>
2022-12-01 06:44:46 +0800

    作业

--- U2/U2-L12/#21.1/#21.1.10/#21.1.10.py
+++ U2/U2-L12/#21.1/#21.1.10/#21.1.10.py
@@ -1,2 +1,21 @@
-file = open("bad_boy.py", "rw")
+file = open("bad_boy.py", "r+")
+a = file.readlines()
+count = 0
+line = 0
+for items in a:
+    line += 1
+    alpha = 0
+    if items[0].isalpha():
+        continue
+    for item in items:
+        if item.isalpha():
+            file.write((" " * ((count // 4 + 1) - count)) + items)
+            print((count // 4 + 1) - count)
+            print((" " * ((count // 4 + 1) - count)) + items)
+            break
+        count += 1
+file.close()
+"""
+不用麻烦跑了，这个不对，但是我不会了
+"""
 
