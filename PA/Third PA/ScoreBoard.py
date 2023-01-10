2583e0ca5cb9e69d44c4b5037bf99afa74267055
温昕亮 <xinliang.wen.2001@stu.moonshotacademy.cn>
2023-01-11 07:56:21 +0800

    希望是晚上8点最后一版

--- PA/Third PA/ScoreBoard.py
+++ PA/Third PA/ScoreBoard.py
@@ -1,30 +1,56 @@
 import pygame
+from config import *
 
 
 class Scoreboard(pygame.sprite.Sprite):
-    def __init__(self, images, position):
+    def __init__(self, images, screen_size):
         pygame.sprite.Sprite.__init__(self)
 
+        self.x, self.y = screen_size
+
         self.score = 0
-        self.high_score = 0
+        with open("data/high-score") as f:
+            high_score = int(f.read())
+        self.high_score = high_score
 
         self.images = images
-        self.image_score = self.rect_score = pygame.Surface
-        self.image_high_score = self.rect_high_score = None
-        self.rect_score.right, self.rect_score.top = position
+
+        self.image_score = pygame.Surface((20 * 5, 31))
+        self.rect_score = self.image_score.get_rect()
+
+        self.image_high_score = pygame.Surface((60 + 20 * 5, 31))
+        self.rect_high_score = self.image_high_score.get_rect()
+
+        self.rect_score.right, self.rect_score.top = (self.x * 0.95, self.y * 0.1)
         self.rect_high_score.right, self.rect_high_score.top = self.rect_score.left - 20, self.rect_score.top
 
-    def update(self):
+        self.refresh_rate = REFRESHING_RATE / 2
+        self.refresh_count = 0
+
+    def update(self, current_score, first_time):
+        self.score = int(current_score)
         # stitch current score image
-        self.image_score = pygame.Surface((20 * 5, 31))
+        self.image_score.fill((235, 235, 235))
         for i, _ in enumerate(str(self.score).zfill(5)):  # current score images
             self.image_score.blit(self.images[int(_)], (20 * i, 0))
 
+        if self.score % 100 < 20 and self.score // 100 >= 1:
+
+            if self.refresh_count == self.refresh_rate:
+                print(1111111111111)
+                self.image_score.set_alpha(255)
+                self.refresh_count = 0
+            self.refresh_count += 1
+
         # stitch high score image
-        self.image_high_score = pygame.Surface((60 + 20 * 5, 31))
-        self.image_high_score.blit(self.images[-1], (0, 0))
-        for i, _ in enumerate(str(self.high_score).zfill(5)):  # highest score images
-            self.image_high_score.blit(self.images[int(_)], (60 + 20 * i, 0))
+        self.image_high_score.fill((235, 235, 235))
+        self.image_high_score.set_alpha(190)
+        if first_time == 1:
+            if self.high_score <= self.score:
+                self.high_score = self.score
+            self.image_high_score.blit(self.images[-1], (0, 0))
+            for i, _ in enumerate(str(self.high_score).zfill(5)):  # highest score images
+                self.image_high_score.blit(self.images[int(_)], (60 + 20 * i, 0))
 
     def draw(self, screen):
         screen.blit(self.image_score, self.rect_score)
--- PA/Third PA/end.py
+++ PA/Third PA/end.py
@@ -1,17 +1,37 @@
 import pygame
+from config import *
 
 
 class End(pygame.sprite.Sprite):
-    def __init__(self, image, screen_size=None):
+    def __init__(self, images, screen_size=None):
         pygame.sprite.Sprite.__init__(self)
 
+        self.t = 1
+
         self.x, self.y = screen_size[0], screen_size[1]
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.bottom = (self.x/2, self.y/2)
+        self.images = images
+        self.image_fiction = self.images[1]
+        self.rect = self.image_fiction.get_rect()
+        self.rect.left, self.rect.bottom = ((self.x / 2) - 30, self.y / 2 + 50)
+        self.image_over = self.images[0]
+        self.rect_over = self.image_over.get_rect()
+        self.rect_over.left, self.rect_over.bottom = ((self.x / 2) - 185, (self.y / 2) - 50)
+
+        self.refresh_rate = REFRESHING_RATE
+        self.refresh_counter = 0
+        self.hold_counter = REFRESHING_RATE * 2
+
+    def update(self):
+        self.image_fiction = self.images[self.t]
+        self.refresh_counter += 1
+
+        if self.refresh_counter == self.refresh_rate and self.t < 8:
+            self.t += 1
+            self.refresh_counter = 0
 
     def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        screen.blit(self.image_fiction, self.rect)
+        screen.blit(self.image_over, self.rect_over)
 
 
 
